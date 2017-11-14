99188904222eb691a8934f8090e194ba8067fb91
lozeki <minhtri.ng@gmail.com>
2017-11-14 17:43:50 -0500

    commit
    Change the main function - add command "battle"

--- src/game.java
+++ src/game.java
@@ -4,16 +4,18 @@ public class game {
 	public static void main(String[] args) {
 		// Use case - Player attacks monster
 		Player p = new Player("The Lord",100,10);
-		Monster m = new Monster("Spider",50,5);		
-		p.battle(m); // "The Lord" attacks "Spider" 
+		Monster m = new Monster("Spider",50,5);			
 		
 		boolean exit = false;
 		Scanner reader = new Scanner(System.in);
 		String command = "";
+		System.out.println ("Please enter a command");
 		while (!exit) {
 			command = reader.next();
 			if (command.equals("quit")) {
 				exit = true;
+			}else if (command.equals("battle")) { // "The Lord" attacks "Spider" 
+				p.battle(m);
 			}
 			else {
 				System.out.println("unknown command");
