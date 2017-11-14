d0889dfdd7327692349085e7275eb1b8471b3bf8
djv44 <djv44unh@gmail.com>
2017-11-13 23:10:28 -0500

    first commit
    new changes

---
+++ src/destination.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class destination {
+	public static void main(String[] args) {
+		int North;
+		int South;
+		int East;
+		int West;		
+		
+		System.out.println("Enter your direction of travel");
+		Scanner s1=new Scanner(System.in);
+		int move=s1.nextInt();
+		
+		North = 1;
+		South = 2;
+		East = 3;
+		West = 4; 
+		
+		if (move >= 1) {
+			System.out.println( "Lets Go");
+			
+		}
+		
+		else {
+			System.out.println( "Invalid Selection");
+			
+		}
+		
+	}
+	
+	
+}
---
+++ src/playermove.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class playermove {
+
+	public static void main(String[] args) {
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter your destination");
+		String f = s1.next();
+		System.out.println("Lets Go " + f);
+	}
+
+}
+
