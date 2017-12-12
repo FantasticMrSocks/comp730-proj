b9b8010fed3afdde9739b55873ae916f300ae488
lozeki <minhtri.ng@gmail.com>
2017-12-12 16:11:39 -0500

    add demo data, use items and usecase finish game without condition

--- src/game.java
+++ src/game.java
@@ -3,29 +3,41 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class game {
-	static final int maxWidth = 3;
-	static final int maxHeight = 3;
-	static final int maxRooms = 9;
+
 	public static void main(String[] args) {		
 		// Use case - Player attacks monster
 		List<Monster> monsters = new ArrayList<Monster>(); // list of monsters
-		Player p = new Player("The Lord",100,10,50);
-		Monster m = new Monster("Spider",50,5,50);
-		monsters.add(new Monster("Monkey",40,3,50));
-		
 		List<Room> rooms = new ArrayList<Room>();
+		Player p = new Player("The Lord",100,10,50);
+		List<Inspectable> items= new ArrayList<Inspectable>();; // list of items
 		
-		rooms.add(new Room());
-		rooms.add(new Room());
-		rooms.add(new Room());
-		rooms.add(new Room());
-		rooms.add(new Room());
-		rooms.add(new Room());
-		rooms.add(new Room());
-		rooms.add(new Room());
-		rooms.add(new Room());
-		rooms.add(new Room());
+
+
+		monsters.add(new Monster("Monkey",40,3,50));	
+		monsters.add(new Monster("Spider",20,2,30));	
+		monsters.add(new Monster("CrazyDog",50,4,40));	
+		monsters.add(new Monster("Slime",10,2,20));
 		
+		items.add(new Inspectable ("bread","an old bread"));
+		items.add(new Inspectable ("lettuce", "some green to eat"));
+		items.add(new Inspectable ("pepper", "you always need pepper"));
+		items.add(new Inspectable ("salt", "don't add too much"));
+		items.add(new Inspectable ("turkey", "of the sliced variety"));
+		items.add(new Inspectable ("banana","an edible fruit"));
+		items.add(new Inspectable ("apple","a deciduous fruit"));		
+		items.add(new Inspectable ("meat","Raw meat can be cooked or eaten"));
+
+		rooms.add(new Room());
+		rooms.add(new Room());
+		rooms.add(new Room());
+		rooms.add(new Room());
+		rooms.add(new Room());
+		rooms.add(new Room());
+		rooms.add(new Room());
+		rooms.add(new Room());
+		rooms.add(new Room());
+		rooms.add(new Room());
+
 		rooms.get(0).addHallway("east", rooms.get(1));
 		rooms.get(1).addHallway("west", rooms.get(0));	
 		rooms.get(1).addHallway("north", rooms.get(2));
@@ -38,7 +50,7 @@ public class game {
 		rooms.get(7).addHallway("north", rooms.get(8));
 		rooms.get(8).addHallway("up", rooms.get(9));
 		rooms.get(9).addHallway("up", rooms.get(6));
-		
+
 		rooms.get(6).addHallway("southwest", rooms.get(4));
 		rooms.get(6).addHallway("southeast", rooms.get(5));	
 		rooms.get(5).addHallway("southwest", rooms.get(4));
@@ -49,33 +61,36 @@ public class game {
 		rooms.get(8).addHallway("south", rooms.get(7));
 		rooms.get(9).addHallway("down", rooms.get(8));
 		rooms.get(6).addHallway("east", rooms.get(9));
-		
+
 		rooms.get(0).addInformation("Restroom","A bathroom in a public building. I should get out of here and go explore. To the east there is a door leading outside. As the saying goes, 'There's nothing to see here.'");// add name and description to a room
 		rooms.get(1).addInformation("Field", "A green field with nothing in it. To the southwest is the restroom, to the north is a door leading inside, to the east is the Garden.");// add name and description to a room
 		rooms.get(2).addInformation("Storage", "An old dusty storage room with a shiny object. To the west is the kitchen, we can also go south to go back outside.");// add name and description to a room
 		rooms.get(3).addInformation("Kitchen", "The kitchen, what messey people eat in this place. To the north is an office and to the southeast is the Storage closet."); // add name and description to a room
-		rooms.get(4).addInformation("Office", "There are many desks and chairs here. We can move northeast to the Auditorium or east to the Meeting Room. We also can go south back to the kitchen.");// add name and description to a room
-		rooms.get(5).addInformation("Meeting Room", "This is a meeting room, there is one large desk, many chairs, and a file drawer. The Auditorium is to the northwest and the Office to the southwest.");//add name and description to a room
+		rooms.get(4).addInformation("Office", "There are many desks and chairs here. We can move northeast to the Auditorium or east to the Meeting Roomonsters. We also can go south back to the kitchen.");// add name and description to a room
+		rooms.get(5).addInformation("Meeting Room", "This is a meeting room, there is one large desk, many chairs, and a file drawerooms. The Auditorium is to the northwest and the Office to the southwest.");//add name and description to a room
 		rooms.get(6).addInformation("Auditorium", "The auditorium is a huge space, something big with six legs is blocking the exit. We have the Office to the southwest and the Meeting Room to the southeast, and the Fire Ladder outside the window to the east");//add description to a room
 		rooms.get(7).addInformation("Garden", "This is the garden, to the north is a fire escape, to the southwest is the field");
 		rooms.get(8).addInformation("Fire Escape", "This is the outside Fire Escape, we can climb up the ladder in front of us or go south back to the Garden");
 		rooms.get(9).addInformation("Fire Ladder", "This is the Fire Ladder, we can climb up and into the Auditorium or climb down to the bottom of the Fire Escape");
 
+		rooms.get(0).addItem(items.get(0)); // add a bread into the first room inventory
+		rooms.get(7).addItem(items.get(1)); // add lettuce into the garden inventory
+		rooms.get(3).addItem(items.get(2)); // add pepper into the kitchen inventory
+		rooms.get(5).addItem(items.get(3));
+		rooms.get(9).addItem(items.get(4));
 		
+		p.setRoom(rooms.get(0)); // set the default room for player		
 		
-		rooms.get(0).addItem(new Inspectable ("Bread","an old bread")); // add a bread into the first room inventory
-		rooms.get(7).addItem(new Inspectable ("lettuce", "some green to eat")); // add lettuce into the garden inventory
-		rooms.get(3).addItem(new Inspectable ("pepper", "you always need pepper")); // add pepper into the kitchen inventory
-		rooms.get(5).addItem(new Inspectable ("salt", "don't add too much"));
-		rooms.get(9).addItem(new Inspectable ("turkey", "of the sliced variety"));
-		p.addItem(new Inspectable ("Apple","a deciduous fruit")); // add an Apple into player inventory
+		monsters.get(0).setRoom(rooms.get(1)); // The Monkey is in room 1
+		monsters.get(1).setRoom(rooms.get(2)); // The Spider is in room 2
+		monsters.get(2).setRoom(rooms.get(4)); // The Crazy Dog is in room 4
+		monsters.get(3).setRoom(rooms.get(6)); // The Slime is in room 6
 		
-		p.setRoom(rooms.get(0));
-		System.out.println(p.inspect()); // inspect player
-		//p.move("east");                   // player move to east
-		//System.out.println(p.inspect());
-		monsters.get(0).setRoom(rooms.get(0));
-		monsters.get(0).addItem(new Inspectable ("Banana","an edible fruit")); // add an banana to the monkey
+		monsters.get(0).addItem(items.get(5)); // add a banana to the monkey
+		monsters.get(0).addItem(items.get(6)); // add an Apple to the Spider
+		monsters.get(2).addItem(items.get(7)); // add a raw meat to the dog
+		monsters.get(3).addItem(items.get(5)); // add an Banana to the Slime
+		
 		boolean exit = false;
 		Scanner reader = new Scanner(System.in);
 		String command ;
@@ -83,8 +98,13 @@ public class game {
 		//player interaction happens here
 		while (!exit) {
 			command = reader.nextLine();
-			if (command.toLowerCase().equals("quit")) {
+			
+			if (command.toLowerCase().equals("make sandwich")&&p.getRoom()==rooms.get(6)) {
+				System.out.println("Congratulation! You are winning the game!!!!!!!\n\n           --GAMEOVER--");
 				exit = true;
+			}
+			else if (command.toLowerCase().equals("help")) {
+				help();
 			}
 			else if (command.toLowerCase().equals("battle")) { // "The Lord" attacks "Spider" 
 				if (findMonster(p,monsters)!=null) 
@@ -130,12 +150,25 @@ public class game {
 			{
 				System.out.println(p.inspect());	
 			}
+			else if 
+			(command.length()>5 && (command.substring(0,3).equals("use"))){
+				String i = command.substring(4,command.length());
+				if(i.equals("banana")||i.equals("apple")||i.equals("bread")||i.equals("meat")||i.equals("turkey")) {
+					p.setHP(p.getHP()+ 30);
+					System.out.println("You restore 30 HP !!!");
+				}
+				else 
+				{
+					System.out.println("This item can't be use");
+				}				
+			}
 			else{
 				System.out.println("unknown command");
 			}
 		}
 		reader.close();
 	}
+	
 	
 	public static Character findMonster(Player p,List<Monster> monsters) {
 		for(Monster m : monsters) {
